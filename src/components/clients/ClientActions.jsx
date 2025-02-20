bd0e7887f3e7289d28871bb308582175e8d176ac
PIYuusHYADAV <161058112+PIYuusHYADAV@users.noreply.github.com>
2025-02-20 20:54:11 +0530

    Update ClientActions.jsx

--- src/components/clients/ClientActions.jsx
+++ src/components/clients/ClientActions.jsx
@@ -18,7 +18,7 @@ const ClientActions = ({ client, onEdit, onDelete }) => {
   const handleDelete = () => {
     if (window.confirm("Are you sure you want to delete this client?")) {
       axios
-        .delete(`https://first-work-2.onrender.com/api/clients/delete/${client.id}`)
+        .delete(`http://54.167.18.161/api/clients/delete/${client.id}`)
         .then(() => {
           onDelete(client.id);
         })
