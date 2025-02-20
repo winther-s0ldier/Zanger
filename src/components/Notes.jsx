605195196db289e7f9f51b433147210accad8fd1
Rudra <rudrakumar1612@gmail.com>
2025-02-20 15:23:44 +0530

    Update Notes.jsx

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -10,7 +10,7 @@ function App() {
 
   useEffect(() => {
     axios
-      .get(`https://first-work-2.onrender.com/data?username=${loggedInUserName.user.email}`)
+      .get(`http://54.167.18.161/data?username=${loggedInUserName.user.email}`)
       .then((res) => {
         setnotes(res.data);
         setcurrentnote(res.data[0] || null);
@@ -43,7 +43,7 @@ function App() {
                     author: loggedInUserName.user.email,
                   };
 
-                  await axios.post("https://first-work-2.onrender.com/add", newNote);
+                  await axios.post("http://54.167.18.161/add", newNote);
 
                   setnotes([...notes, newNote]);
                   setcurrentnote(newNote);
@@ -81,7 +81,7 @@ function App() {
                         setnotes(notes.filter((n) => n.id !== note.id));
                         setcurrentnote(null);
                         await axios.delete(
-                          `https://first-work-2.onrender.com/delete/${note.id}`,
+                          `http://54.167.18.161/delete/${note.id}`,
                           currnote
                         );
                       }}
@@ -121,7 +121,7 @@ function App() {
                         )
                       );
                       await axios.put(
-                        `https://first-work-2.onrender.com/update/${currnote.id}`,
+                        `http://54.167.18.161/update/${currnote.id}`,
                         currnote
                       );
                     }
@@ -166,7 +166,7 @@ function App() {
                       )
                     );
                     await axios.put(
-                      `https://first-work-2.onrender.com/update/${currnote.id}`,
+                      `http://54.167.18.161/update/${currnote.id}`,
                       currnote
                     );
                   }
