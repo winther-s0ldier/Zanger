5b16ad44f8bf553de03dae97c1c10638416c2d38
PIYuusHYADAV <161058112+PIYuusHYADAV@users.noreply.github.com>
2025-02-20 22:56:39 +0530

    Update AddClientModal.jsx

--- src/components/clients/AddClientModal.jsx
+++ src/components/clients/AddClientModal.jsx
@@ -17,7 +17,7 @@ const AddClientModal = ({ onClose, onAdd }) => {
 
     try {
       const response = await axios.post(
-        "http://54.167.18.161/api/clients/add", // Replace with actual API endpoint
+        "http://54.234.253.93/api/clients/add", // Replace with actual API endpoint
         formData
       );
 
